26632622898d923f8c868c45fa88870961ccadee
gupta-tilak <thetilakgupta@gmail.com>
2025-05-14 19:46:14 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building a ML project to predict delivery time for food delivery apps in minutes.',
+    author='Tilak',
+    license='',
+)
